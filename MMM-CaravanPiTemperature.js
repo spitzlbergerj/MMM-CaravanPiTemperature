75f2e95d12ec16efe933af5dc83bf596bfc313e2
Josef Spitzlberger <38983450+spitzlbergerj@users.noreply.github.com>
2019-11-05 12:24:16 +0100

    Update MMM-CaravanPiTemperature.js

--- MMM-CaravanPiTemperature.js
+++ MMM-CaravanPiTemperature.js
@@ -14,7 +14,7 @@ Module.register("MMM-CaravanPiTemperature",{
 
 defaults:{
 	valueDir: "/home/pi/CaravanPi/values",
-	updateInterval: 100000, // milliseconds
+	updateInterval: 300000, // milliseconds
 	tempUnit: " °C",
 	tempPrecision: 2,
 	showDate: true,
